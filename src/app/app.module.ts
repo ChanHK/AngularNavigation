e65cf46f26c6b832760353916be5c60497f8fc5b
ChanHK <59449120+ChanHK@users.noreply.github.com>
2021-10-09 19:27:40 +0800

    create pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
+import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    ProductDetailsComponent,
+    NoPageFoundComponent
   ],
   imports: [
     BrowserModule,

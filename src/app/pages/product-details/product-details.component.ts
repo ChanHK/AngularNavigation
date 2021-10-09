c91ce419831725f974780121cfb9214137ab6acc
ChanHK <59449120+ChanHK@users.noreply.github.com>
2021-10-09 22:15:09 +0800

    update

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
+  productID: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private actRoute: ActivatedRoute) {
+    this.productID = this.actRoute.snapshot.params.id;
   }
 
+  ngOnInit(): void {}
 }
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
+  Products = [
+    {
+      id: 'ABC123',
+      name: 'Basketballs',
+      description: 'Lorem ipsum.',
+    },
+    {
+      id: 'DEF456',
+      name: 'Gloves',
+      description: 'Lorem ipsum.',
+    },
+    {
+      id: 'GHI789',
+      name: 'Bags',
+      description: 'Lorem ipsum.',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
